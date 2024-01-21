d76565918f0d001f0f386fb0065e61e9a74c4645
IreneBenito30 <irenebenitocedillo01@gmail.com>
2024-01-21 14:29:53 +0100

    archivo index terminado

--- scripts/index.sh
+++ scripts/index.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # This script should index the genome file specified in the first argument ($1),
 # creating the index in a directory specified by the second argument ($2).
 
@@ -6,3 +8,34 @@
 
 # STAR --runThreadN 4 --runMode genomeGenerate --genomeDir <outdir> \
 # --genomeFastaFiles <genomefile> --genomeSAindexNbases 9
+
+if [ "$#" -ne 2 ]; then
+    echo "Uso: $0 <archivo_genoma> <directorio_salida>"
+    exit 1
+fi
+
+# Asigna argumentos a variables
+archivo_genoma=$1
+directorio_salida=$2
+
+# Verifica si el archivo de genoma existe
+if [ ! -f "$archivo_genoma" ]; then
+    echo "Error: El archivo de genoma '$archivo_genoma' no existe."
+    exit 1
+fi
+
+# Verifica si directorio_salida tiene un valor válido
+if [ -z "$directorio_salida" ]; then
+    echo "Error: La variable directorio_salida está vacía."
+    exit 1
+fi
+
+# Crear el directorio de salida si no existe
+mkdir -p "$directorio_salida"
+
+# Comando STAR para generar el índice del genoma
+STAR --runThreadN 4 --runMode genomeGenerate --genomeDir "$directorio_salida" \
+--genomeFastaFiles "$archivo_genoma" --genomeSAindexNbases 9
+
+
+
