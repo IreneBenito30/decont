2ace79af8268df74f76e77280f4f7a4bf8148b0c
IreneBenito30 <irenebenitocedillo01@gmail.com>
2024-01-31 02:09:23 +0100

    pipeline terminado

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -1,32 +1,46 @@
 #!/bin/bash
-
-
-
-
-'''
 # Comprobación de que se proporcionan los argumentos necesarios
+
 if [ "$#" -ne 2 ]; then
     echo "Usage : $0 <list_of_urls> <contaminants_url>"
     exit 1
 fi
 
+echo "comprobacion de argumentos correcta" >> ~/decont/log/pipeline.log
+
+
+# ./scripts/pipeline.sh /home/vant/decont/data/urls  https://bioinformatics.cnio.es/data/courses/decont/contaminants.fasta.gz
+
+
+
+
 # Este script recibe 2 argumentos que los guardamos en las siguientes variables
 list_of_urls=$(cat $1)
 contaminants_url=$2
+
+
+
 
 # Descargar todos los archivos especificados en list_of_urls
 for url in $list_of_urls; do
     bash scripts/download.sh $url ~/decont/data no another
 done
 
+echo "descarga de urls" >> ~/decont/log/pipeline.log
+
 # Descargar el archivo fasta de contaminantes, descomprimirlo y filtrar pequeñas RNA nucleares
-echo "Descargando la base de datos de contaminantes y filtrando pequeñas RNA nucleares..."
+echo "Descargando la base de datos de contaminantes y filtrando pequeñas RNA nucleares..." >> ~/decont/log/pipeline.log
 
 bash scripts/download.sh $contaminants_url ~/decont/res yes another
 bash scripts/download.sh $contaminants_url ~/decont/res no another
 
 
+echo "Descarga de base de datos de contaminantes" >> ~/decont/log/pipeline.log
+
+
 bash scripts/index.sh ~/decont/res/contaminants.fasta ~/decont/res/contaminants_idx
+
+echo "indexacion completada" >> ~/decont/log/pipeline.log
 
 
 
@@ -42,6 +56,7 @@ for sid in $list_of_sample_ids; do
     bash scripts/merge_fastqs.sh ~/decont/data ~/decont/out/merged $sid
 done
 
+echo "Fusion de archivos completada" >> ~/decont/log/pipeline.log
 
 
 
@@ -65,43 +80,40 @@ for sid in $list_of_sample_ids; do
         > ~/decont/log/cutadapt/"$sid".log
 done
 
+echo "cutadapt completado" >> ~/decont/log/pipeline.log
 
-
-'''
 
 # TODO: run STAR for all trimmed files
 
-echo "******************************comienza el STAR************************************+"
 
 for fname in out/trimmed/*.fastq.gz
 do
     # you will need to obtain the sample ID from the filename
 
+    base=$(basename  "$fname" )
 
-    sid=#TODO
+    sid=${base%%.*}
 
+
+
+    echo "el sid es $sid y el fname $fname"
 
 
      mkdir -p out/star/$sid
 
 
 
-
-
     STAR --runThreadN 4 --genomeDir res/contaminants_idx \
-       --outReadsUnmapped Fastx --readFilesIn <input_file> \
-        --readFilesCommand gunzip -c --outFileNamePrefix <output_directory>
+       --outReadsUnmapped Fastx --readFilesIn ~/decont/out/trimmed/"$sid".trimmed.fastq.gz \
+        --readFilesCommand gunzip -c --outFileNamePrefix "out/star/$sid/"
 done 
 
 
+echo "ejecucion de STAR completada" >> ~/decont/log/pipeline.log
 
 
 
-
-
-
-
-
+echo "Pipeline completado exitosamente." >> ~/decont/log/pipeline.log
 
 
 '''
@@ -112,7 +124,7 @@ echo "Creando archivo de log general..."
 cutadapt_logs=$(cat ~/decont/log/cutadapt/*.log | grep "Reads with adapters" | grep "Total basepairs")
 star_logs=$(cat ~/decont/out/star/*/Log.final.out | grep -E "Uniquely mapped reads %|Number of reads mapped to multiple loci %|Number of reads mapped to too many loci %")
 
-echo -e "Cutadapt Logs:\n$cutadapt_logs\n\nSTAR Logs:\n$star_logs" > ~/decont/log/pipeline.log
+echo -e "Cutadapt Logs:\n$cutadapt_logs\n\nSTAR Logs:\n$star_logs" > ~/decont/log/~/decont/log/pipeline.log
 
 echo "Pipeline completado exitosamente."
 
